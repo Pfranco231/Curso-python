81fc75fe599eea5847c0afc60180167b6a9b3f18
Franco Papeschi <papeschifranco@gmail.com>
2025-06-17 17:13:06 -0300

    preentregable2 listo

---
+++ pre-entregable2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Preentregable-2",
+    version="0.1",
+    description="Entregable 2 Coderhouse",
+    author="Franco Papeschi",
+    author_email="papeschifranco@gmail.com",
+    packages=["Preentregable-2"]
+)
